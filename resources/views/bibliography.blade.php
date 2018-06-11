b14cdda0bfc40e226082ed8fa14b7d9a81f6e323
Unknown <samduemler@gmail.com>
2018-06-11 09:50:14 -0500

    Added bibliography.blade.php

---
+++ resources/views/bibliography.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+@section('title', 'Home')
+@section('css')
+
+@endsection
+@section('js')
+
+@endsection
+@section('content')
+
+
+<h1><strong>Bibliography</strong></h1>
+
+
+
+@endsection
