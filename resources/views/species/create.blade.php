115354048fe25ebb4bf51a84963828177e47d228
Colin Evans <evansct@miamioh.edu>
2018-06-19 16:44:32 -0400

    Edited the layout of the create page
    Once again, not functional. I would like to meet up over skype or
    something to talk. I want to code this myself, but in order to do
    so I need your help.

--- resources/views/species/create.blade.php
+++ resources/views/species/create.blade.php
@@ -21,6 +21,9 @@
 
     {{Form::open(['route' => 'species.store', 'files' => true])}}
     <div class="row">
+         <?php 
+            $count = 0;
+         ?>
         @foreach ($schemeArr as $scheme)
             @if ($scheme->type == 'input')
                 <div class="col-xl-3 col-lg-4 col-md-6 col-xs-12">
@@ -37,23 +40,164 @@
                     </div>
                 </div>
             @elseif ($scheme->type == 'boolean')
-                <div class="col-lg-3 col-md-4 col-xs-6">
+               
+<!--                So that you can say the new species has these uses-->
+                @if($scheme->category == 'uses' && $count == 0)
                     <div class="form-group">
-                        <div class="row">
-                            <div class="col-12">
-                                {{Form::label($scheme->key, $scheme->displayed_name)}}
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12">
-                                Yes
-                                {{Form::radio($scheme->key, 'TRUE')}}
-                                No
-                                {{Form::radio($scheme->key, 'FALSE', true)}}
-                            </div>
+			             <div style="margin: 5px;">
+				         <strong>Uses: </strong>
+                         <br>
+                          <?php 
+                          //Creates an array that stores all the uses
+                            $uses = array();
+                          ?>
+                            @foreach($schemeArr as $scheme)
+                                @if($scheme->category == 'uses')
+                                    <?php 
+                                        //While it's looping through the schemes array, if it finds anything with the category of uses, adds to the array
+                                        $uses[$scheme->key] = $scheme->name;
+                                    ?>
+                                @endif
+
+                            @endforeach
+        <!--                  Takes the array into account for the selection and displays all the values- -->
+                            @foreach($uses as $key => $name)
+                                {{Form::checkbox($key, 'TRUE')}}
+                                {{$name}}
+                                <br>
+                            @endforeach
+                             <?php 
+                                $count++;
+                            ?>
                         </div>
                     </div>
-                </div>
+                @endif
+<!--                So that you can say the new species is found in these habitats-->
+                @if($scheme->category == 'habitat' && $count == 1)
+                   <div class="form-group">
+			            <div style="margin: 5px;">
+				        <strong>Habitats: </strong>
+                        <br>
+                         <?php 
+                         //Creates an array that stores all the uses
+                           $habitat = array();
+                           ?>
+                           @foreach($schemeArr as $scheme)
+                               @if($scheme->category == 'habitat')
+                                   <?php 
+                                       //While it's looping through the schemes array, if it finds anything with the category of uses, adds to the array
+                                       $habitat[$scheme->key] = $scheme->name;
+                                   ?>
+                               @endif
+
+                           @endforeach
+        <!--                 Takes the array into account for the selection and displays all the values- -->
+                           @foreach($habitat as $key => $name)
+                               {{Form::checkbox($key, 'TRUE')}}
+                               {{$name}}
+                               <br>
+                           @endforeach
+                             <?php 
+                                $count++;
+                            ?>
+                       </div>
+                    </div>
+                @endif
+                <!--                so that you can choose if a new species is at which locations-->
+
+                @if($scheme->category == 'locations' && $count == 2)
+                   <div class="form-group">
+			            <div style="margin: 5px;">
+				        <strong>Locations: </strong>
+                   <br>
+                         <?php 
+                         //Creates an array that stores all the uses
+                           $locations = array();
+                           ?>
+                           @foreach($schemeArr as $scheme)
+                               @if($scheme->category == 'locations')
+                                   <?php 
+                                       //While it's looping through the schemes array, if it finds anything with the category of uses, adds to the array
+                                       $locations[$scheme->key] = $scheme->name;
+                                   ?>
+                               @endif
+
+                           @endforeach
+        <!--                 Takes the array into account for the selection and displays all the values- -->
+                           @foreach($locations as $key => $name)
+                               {{Form::checkbox($key, 'TRUE')}}
+                               {{$name}}
+                               <br>
+                           @endforeach
+                             <?php 
+                                $count++;
+                            ?>
+                       </div>
+                    </div>
+                @endif
+<!--                so that you can choose if a new species has which growth forms-->
+                @if($scheme->category == 'growth_form' && $count == 3)
+                   <div class="form-group">
+			            <div style="margin: 5px;">
+				        <strong>Growth Forms: </strong>
+                   <br>
+                         <?php 
+                         //Creates an array that stores all the uses
+                           $growth_forms = array();
+                           ?>
+                               @foreach($schemeArr as $scheme)
+                                   @if($scheme->category == 'growth_form')
+                                       <?php 
+                                           //While it's looping through the schemes array, if it finds anything with the category of uses, adds to the array
+                                           $growth_forms[$scheme->key] = $scheme->name;
+                                       ?>
+                                   @endif
+
+                               @endforeach
+            <!--                 Takes the array into account for the selection and displays all the values- -->
+                               @foreach($growth_forms as $key => $name)
+                                   {{Form::checkbox($key, 'TRUE')}}
+                                   {{$name}}
+                                   <br>
+                               @endforeach
+                             <?php 
+                                $count++;
+                            ?>
+                       </div>
+                    </div>
+                @endif
+<!--                So that you can account for all harvest seasons-->
+                @if($scheme->category == 'season' && $count == 4)
+                   <div class="form-group">
+			            <div style="margin: 5px;">
+				        <strong>Seasons: </strong>
+                   <br>
+                         <?php 
+                         //Creates an array that stores all the uses
+                           $season = array();
+                           ?>
+                               @foreach($schemeArr as $scheme)
+                                   @if($scheme->category == 'season')
+                                       <?php 
+                                           //While it's looping through the schemes array, if it finds anything with the category of uses, adds to the array
+                                           $season[$scheme->key] = $scheme->name;
+                                       ?>
+                                   @endif
+
+                               @endforeach
+            <!--                 Takes the array into account for the selection and displays all the values- -->
+                               @foreach($season as $key => $name)
+                                   {{Form::checkbox($key, 'TRUE')}}
+                                   {{$name}}
+                                   <br>
+                               @endforeach
+                             <?php 
+                                $count++;
+                            ?>
+                       </div>
+                    </div>
+                @endif
+
             @elseif ($scheme->type == 'photo' || $scheme->type == 'audio')
                 <div class="col-md-6 col-xs-12">
 
