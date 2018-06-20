09beebe54ee14ecd7204618c411918ab6f5edfc4
Unknown <evansct@miamioh.edu>
2018-06-20 18:09:18 -0400

    Edited footer
    Added the submit feedback email link to the footer

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -4,7 +4,8 @@
             <div class="col-12" style="text-align: center; color: grey;">
                 &copy; 2018 <a href="http://myaamiacenter.org/">Myaamia Center</a> &middot; 200 Bonham House &middot; <a
                         href="http://miamioh.edu/">Miami University</a> &middot; Oxford &middot; OH &middot; 45056 &middot;
-                <a href="tel:513-529-5648">513-529-5648</a>
+                <a href="tel:513-529-5648">513-529-5648</a> &middot; 
+                <a href="mailto:myaamiacenter@miamioh.edu?subject=Ethnobotanical Database Feedback">Submit Feedback</a>
             </div>
         </div>
     </div>
