8a4306e23a93ad820c26d456677f5aefe39f4559
Sam Duemler <samduemler@gmail.com>
2018-11-14 15:07:28 -0500

    Updated footer text coloring
    Changed most of the footer text to white in order for it to be more visible against the black, and changed the sign in/out text to be green so the user knows that they can click on it.

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -1,14 +1,14 @@
 <footer class="footer">
     <div class="container">
         <div class="row">
-            <div class="col-12" style="text-align: center; color: grey;">
+            <div class="col-12" style="text-align: center; color: white;">
                 &copy; 2018 Miami Tribe of Oklahoma &#124; 3410 P Street &#124; Miami, OK 74354 &#124; 513-529-5648 &#124;
                 @if (Auth::guest())
-                <a style="color: darkslategrey" href="{{url('/login')}}">Sign-in</a>
+                <a style="color: green" href="{{url('/login')}}">Sign-in</a>
                 @else
                 <a href="{{ route('logout.logout') }}"
                         onclick="event.preventDefault();
-                        document.getElementById('logout-form').submit();">
+                        document.getElementById('logout-form').submit();" style="color: green;">
                             Logout ({{ Auth::user()->name }})
                         </a>
 
